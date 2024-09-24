4f835510bc252923936f33d9bacd4035565a34f2
Danny Shen <shenfive@gmail.com>
2024-09-24 10:15:13 +0800

    Initial Commit

---
+++ HelloLocationUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloLocationUIKit
+//
+//  Created by 申潤五 on 2024/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
